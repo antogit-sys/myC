a09bc4e2db0a7efb92c422397f224d3ed158b842
0xdeadc0de <64706936+antogit-sys@users.noreply.github.com>
2021-01-07 23:40:21 +0100

    Update mainstrundef.c

--- esempi/mainstrundef.c
+++ esempi/mainstrundef.c
@@ -7,15 +7,14 @@ int main(void)
 	String strout=new_init();
 	String strin=new_init();
 	
-
 	str_input(strout);
 	puts(strout);
 	str_fout(strout,"fileC.txt");
 	str_fin(strout,"fileC.txt");
+	
 	printf("strin: %s",strin);
-	puts("\npremere per pulire schermo");
+	puts("press the key...");
 	pause();
-	clrscr();
 	
 	
 return 0;
