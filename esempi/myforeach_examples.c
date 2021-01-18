0b8e5dfcd53a79881518bfbf77b2f3014c8a9e95
0xdeadc0de <64706936+antogit-sys@users.noreply.github.com>
2021-01-18 23:56:58 +0100

    Create myforeach_examples.c

---
+++ esempi/myforeach_examples.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../myLibc/_general.h"
+int main(void)
+{
+  int *v1=ANEW(int,10);
+	int *v2=ANEW(int,10);
+	int v3[10];
+	
+	/*for normale*/
+	puts("for =");
+	for(size_t i=0; i<10; ++i){
+		v1[i]=i;
+		printf("%d ",*(v1+i));	
+	}
+	
+	/*foreach funzionante su tutto*/
+	putchar('\n');
+	putchar('\n');
+	puts("foreach =");
+	foreach(size_t i=0, i<10){
+		v2[i]=i;
+		printf("%d ",*(v2+i));	
+	}
+	
+	/*foreach funzionante solo per array statici*/
+	putchar('\n');
+	putchar('\n');
+	puts("foreach_static_array =");
+	foreach_static_array(size_t,i, v3){
+		v3[i]=i;
+		printf("%d ",v3[i]);	
+	}
+		
+return EXIT_SUCCESS;
+}
+
