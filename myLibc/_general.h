8ff00418c8a413833e5c79202a0792514a244ad9
0xdeadc0de <64706936+antogit-sys@users.noreply.github.com>
2021-01-18 21:23:07 +0100

    Update _general.h

--- myLibc/_general.h
+++ myLibc/_general.h
@@ -2,7 +2,11 @@
 #	define _MYSTDLIB_H_
 #	include <stdlib.h>
 #	define ANEW(T,N) 		(T*)malloc(sizeof(T)*N)		
-#	define NEW(T)			(T*)malloc(sizeof(T))
+#	define NEW(T)			  (T*)malloc(sizeof(T))
 #endif
 
-#define cpause()	while(!getchar())	//while(getchar()!='\n')
+#define cpause()    while(!getchar())	//while(getchar()!='\n')
+
+#define foreach(index, increment)	    for(index-1; ++increment; )
+#define dim_array(v)					        (sizeof(v) / sizeof(v[0]))
+#define	foreach_array(type, var, v)		foreach(type var=0, var<dim_array(v))
