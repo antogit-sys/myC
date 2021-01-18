12b247fc657b9132a2d3d365b80d78cb176e759b
0xdeadc0de <64706936+antogit-sys@users.noreply.github.com>
2021-01-18 23:17:14 +0100

    Update _general.h

--- myLibc/_general.h
+++ myLibc/_general.h
@@ -1,8 +1,8 @@
 #ifndef _MYSTDLIB_H_
 #	define _MYSTDLIB_H_
 #	include <stdlib.h>
-#	define ANEW(T,N) 		(T*)malloc(sizeof(T)*N)		
-#	define NEW(T)			  (T*)malloc(sizeof(T))
+#	define ANEW(T,N)  (T*)malloc(sizeof(T)*N)		
+#	define NEW(T)     (T*)malloc(sizeof(T))
 #endif
 
 #define cpause()    while(!getchar())	//while(getchar()!='\n')
